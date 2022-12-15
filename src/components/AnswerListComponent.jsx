648d39db55ee41c9c7c569f8c90149b01166f330
albinolssonn <56583621+albinolssonn@users.noreply.github.com>
2022-12-15 22:18:50 +0100

    To late for good code

--- src/components/AnswerListComponent.jsx
+++ src/components/AnswerListComponent.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "../stylesheets/AnswerStyles.css";
 
 const AnswerListComponent = () => {
-  return <div className="">AnswerListComponent</div>;
+  const [questions, setQuestions] = useState([]);
+
+  const getAllAnseredQuestions = async () => {
+    const tmpList = await getDocs(qaColRef);
+    setQuestionList(
+      tmpList.docs.map((question) => ({ ...question.data(), id: question.id }))
+    );
+  };
+
+  return (
+    <div className="answer-section">
+      <div className="answer-container">
+        <div className="answer-list">
+          <div className="answer-card"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AnswerListComponent;
